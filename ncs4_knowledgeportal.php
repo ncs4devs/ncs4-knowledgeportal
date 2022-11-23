2481b28782813c82046ac23eb0c639ddeae205f7
Aayush Gautam <w10153693@usm.edu>
2022-11-23 13:20:40 -0600

    Fixed the error obtained while activating the plugin

--- ncs4_knowledgeportal.php
+++ ncs4_knowledgeportal.php
@@ -19,20 +19,28 @@
 // Exit if accessed directly.
 defined( 'ABSPATH' ) || exit;
 
-require_once plugin_dir_path(__FILE__) . '/includes/kp-createmetabox.php';
-require_once plugin_dir_path(__FILE__) . '/includes/kp-registerposttype.php';
 
+require_once plugin_dir_path(__FILE__) . '/includes/kp-registerposttype.php';
 require_once plugin_dir_path(__FILE__) . '/knowledgeportal-templates/dashboard.php';
 
-//Initializing required custom post type and taxonomy
+/**
+ * Initializing required custom post type and taxonomy
+ * Function located in kp-registerposttype in includes
+ */
 add_action('init', 'ncs4_knowledgeportal_register_post_type');
 add_action( 'init', 'ncs4_knowledgeportal_register_taxonomy' );
 
-//Initializing required meta boxes for the custom post type Knowledge Portal Entries 
+/**
+ * Initializing required meta boxes for the custom post type Knowledge Portal Entries 
+ * Function located in kp-registerposttype in includes
+ */
 add_action( 'admin_init', 'ncs4_add_post_meta_boxes' );
 add_action( 'save_post', 'ncs4_save_post_meta_boxes' );
 
-//Creating a shortcode for Dashboard Display
+/**
+ * Creating shortcode so that the perotal can be displayed anywhere in the site
+ * Function located in the dashboard.php file in knowledgeportal-templates
+ */
 add_shortcode('ncs4-knowledgeportal-entries-list', 'ncs4_knowledgeportal_entires')
 
 ?>
