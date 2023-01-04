4254151bffee0ad526f5a202c730f07335440ea0
Aayush Gautam <l65.fread@gmail.com>
2023-01-04 15:29:09 -0600

    Created the top bar, now adding functionality remains

--- templates/dashboard.php
+++ templates/dashboard.php
@@ -5,6 +5,7 @@ function ncs4_knowledgeportal_entires() {
 
 	ob_start();
 	$templates = new KP_Template_Loader;
+	$templates->get_template_part('top-bar');
 	$templates->get_template_part( 'entries-loop' );
     return ob_get_clean();
 }
